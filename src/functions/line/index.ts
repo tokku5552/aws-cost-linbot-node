fa86e726678ee1cdaa6c841c88950f514957a002
tokku5552 <physics.0121@gmail.com>
2021-07-13 04:40:21 +0900

    schmaの記載削除

--- src/functions/line/index.ts
+++ src/functions/line/index.ts
@@ -1,5 +1,5 @@
 import { handlerPath } from "@libs/handlerResolver";
-import schema from "@functions/line/schema";
+// import schema from "@functions/line/schema";
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
@@ -8,11 +8,11 @@ export default {
       http: {
         method: "post",
         path: "line",
-        request: {
-          schemas: {
-            "application/json": schema,
-          },
-        },
+        // request: {
+        //   schemas: {
+        //     "application/json": schema,
+        //   },
+        // },
       },
     },
   ],
